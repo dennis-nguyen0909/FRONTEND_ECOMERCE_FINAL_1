2d2712ac29dc3e52c05beb8f4e253e8a0a538364
QuocAnh <ctmdev03@gmail.com>
2024-12-20 23:04:52 +0700

    add-routes

---
+++ src/routes/index.js
@@ -0,0 +1,50 @@
+import { AdminPage } from "../pages/AdminPage/AdminPage";
+import { HomePage } from "../pages/HomePage/HomePage";
+import { LogInPage } from "../pages/LogInPage/LogInPage";
+import { NotFoundPage } from "../pages/NotFoundPage/NotFoundPage";
+import { ProductDetailPage } from "../pages/ProductDetailPage/ProductDetailPage";
+import { ProductPage } from "../pages/ProductPage/ProductPage";
+import { SignUpPage } from "../pages/SignUpPage/SignUpPage";
+export const routes = [
+  {
+    path: "/",
+    page: HomePage,
+    isShowHeader: true,
+    isShowFooter: true,
+  },
+
+  {
+    path: "/product",
+    page: ProductPage,
+    isShowHeader: true,
+    isShowFooter: false,
+  },
+  {
+    path: "/login",
+    page: LogInPage,
+    isShowHeader: false,
+  },
+  {
+    path: "/sign-up",
+    page: SignUpPage,
+    isShowHeader: false,
+  },
+  {
+    path: "/product-detail/:id",
+    page: ProductDetailPage,
+    isShowHeader: true,
+    isShowFooter: false,
+  },
+
+  {
+    path: "/system/admin",
+    page: AdminPage,
+    isShowHeader: false,
+    isPrivate: true,
+  },
+  {
+    path: "*",
+    page: NotFoundPage,
+    isShowHeader: false,
+  },
+];
